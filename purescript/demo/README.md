ea0f53a338407ec8c6a97bd462363689b8da8f6b
torchhound <jcieslik@umail.iu.edu>
2017-04-03 20:37:45 -0400

    purescript-halogen demo works

---
+++ purescript/demo/README.md
@@ -0,0 +1,7 @@
+# purescript-halogen Demo
+
+# Install
+
+`npm install`
+`bower install`
+`npm run build`
--- purescript/demo/src/Components/Demo.purs
+++ purescript/demo/src/Components/Demo.purs
@@ -1,18 +1,21 @@
 module Components.Demo where
 
--- | import Prelude
-import Halogen as H
-import Halogen.HTML as HH
+import Prelude
 -- | import Halogen.HTML.Properties as HP
 -- | import Halogen.HTML.Events as HE
+import Halogen as H
+import Halogen.HTML as HH
+import Data.Const (Const)
+import Data.Newtype (unwrap)
+import Data.Maybe (Maybe(..))
 
-demo :: forall m. H.Component HH.HTML m
+
+demo :: forall m. H.Component HH.HTML (Const Void) Unit Void m
 demo = 
   H.component
-    { render }
-  where
-
-  render :: H.ComponentHTML
-  render = 
-    HH.div_
-      [ HH.h1_ [ HH.text "Hello Halogen" ] ]
+    { initialState: const unit
+    , render: const $ HH.div_ [ HH.h1_ [ HH.text "Hello Halogen" ] ]
+    , eval: absurd <<< unwrap
+    , receiver: const Nothing
+    }
+    
