adc60a881398b7c1c049e4f26580a8f5cc2060fb
torchhound <unknownmasayoshi@gmail.com>
2018-02-11 16:37:19 -0500

    Added config.js.example to scripts/npmStart.sh

--- scripts/npmStart.sh
+++ scripts/npmStart.sh
@@ -5,6 +5,7 @@ mkdir test
 touch test/tests.js
 touch index.js
 touch config.js
+touch config.js.example
 wget https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore -O .gitignore
 echo "config.js" >> .gitignore
 npm init
